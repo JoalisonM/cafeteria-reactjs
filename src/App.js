3e01f439841ca550d938c77467602d95055fd4c0
Joalison Matheus <joalison.ferreira@academico.ifpb.edu.br>
2022-10-27 16:46:20 -0300

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
+import { useState, useRef } from "react";
 import Food from './components/Food';
-import foods from './models/foods';
+import data from './models/foods';
+import { Button, Container, Row } from "react-bootstrap";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+
+import hamburguerImg from "./imgs/hamburguer.avif";
 
 function App() {
+  const [foods, setFoods] = useState(data);
+  const buttonAdd = useRef(null);
+
+  const handleClick = () => {
+    const food = {
+      id: 7,
+      name: 'Hambúrguer',
+      image: hamburguerImg
+    }
+
+    setFoods([...foods, food]);
+
+    buttonAdd.current.disabled = true;
+  }
+
   return (
-    <>
-      <h1>Menu</h1>
-      <button
-        type="button"
-        className="btn btn-secondary rounded-circle mr-4 font-weight-bold"
-        onClick={() => { }}
-      >
-        +
-      </button>
-      <section className="card-deck my-3">
+    <Container>
+      <h1 className="mt-5 text-center">Menu</h1>
+      <div className="text-end mb-2">
+        <Button
+          variant="secondary"
+          className="rounded-circle mr-4 font-weight-bold"
+          onClick={handleClick}
+          ref={buttonAdd}
+        >
+          +
+        </Button>
+      </div>
+      <Row xs={1} md={2} className="g-4 align-self-stretch">
         {foods.map((food) => (
           <Food food={food} key={food.id} />
         ))}
-      </section>
-    </>
+      </Row>
+    </Container>
   );
 }
 
